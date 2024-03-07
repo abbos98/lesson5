f4280c52857ca097fdc303eb4145344fb39129c5
Abbos <abbos.pankiy2018@gmail.com>
2024-03-07 11:42:18 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////
+// Math.pow //
+
+// function myMathPow(son, darajasi){
+//     return son ** darajasi
+// }
+
+// console.log(myMathPow( 5, 2);
+
+
+///////////////////////////////////////////////
+// MySqrt //
+
+// function myMathsqrt (son-?)
+// function mySqrt(son) {
+//     return son ** 0.5
+// }
+// console.log(Math.sqrt (64));
+
+///////////////////////////////////////////////
+
+// function MyAbs(son) {
+//     son >= 0 && console.log(son);
+//     son < 0 && console.log(son *(-1));
+// }
+// MyAbs(-10)
+
+///////////////////////////////////////////////
+// Math.floor //
+
+// function myFloor(son) {
+//     console.log(parseInt(son));
+// }
+// myFloor(354.99);
+
+
+// // console.log(Math.floor(5.9));
+
+////////////////////////////////////////////////
+// Math.ceil //
+
+// function myMathCeil(son){
+//     son > parseInt(son) && console.log(parseInt(son) + 1);
+//     son === parseInt(son) && console.log(son);
+// }
+// myMathCeil(10.1);
+// myMathCeil(10);
+
+
+// console.log(Math.ceil (12.1));
